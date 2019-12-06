085269453d42311f135d4caa0bb0ddaa2635806f
soil <sh.yoshida@live.jp>
2019-12-06 11:02:48 +0900

    init

---
+++ src/main/java/com/jigowatts/springboot_with_mybatis/SpringBootWithMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.jigowatts.springboot_with_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jigowatts/springboot_with_mybatis/SpringBootWithMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jigowatts.springboot_with_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
