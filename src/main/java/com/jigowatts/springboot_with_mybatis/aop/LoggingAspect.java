d5dc7516dd1a9a55e0c1af03a5f8a4877966f0d8
soil <sh.yoshida@live.jp>
2019-12-20 23:03:23 +0900

    Adviceのバリエーション追加

--- src/main/java/com/jigowatts/springboot_with_mybatis/aop/LoggingAspect.java
+++ src/main/java/com/jigowatts/springboot_with_mybatis/aop/LoggingAspect.java
@@ -1,8 +1,13 @@
 package com.jigowatts.springboot_with_mybatis.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,6 +28,26 @@ public class LoggingAspect {
             e.printStackTrace();
             throw e;
         }
+    }
+
+    @Before("execution(* *..*Service.*(..))")
+    public void startLog(JoinPoint jp) {
+        System.out.println("-->サービス【開始】: " + jp.getSignature());
+    }
+
+    @After("execution(* *..*Service.*(..))")
+    public void endLog(JoinPoint jp) {
+        System.out.println("-->サービス【終了】: " + jp.getSignature());
+    }
+
+    @AfterReturning("execution(* *..*Service.*(..))")
+    public void nomalEndLog(JoinPoint jp) {
+        System.out.println("--->【正常】: " + jp.getSignature());
+    }
+
+    @AfterThrowing("execution(* *..*Service.*(..))")
+    public void exceptionEndLog(JoinPoint jp) {
+        System.out.println("--->【異常】: " + jp.getSignature());
     }
 
 }
