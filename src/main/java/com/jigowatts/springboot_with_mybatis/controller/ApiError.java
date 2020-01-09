f9e16ff9bd6954fc6ce57704f28b4896e6e3397d
soil <sh.yoshida@live.jp>
2020-01-09 22:30:16 +0900

    入力チェックエラーの詳細情報を出力

--- src/main/java/com/jigowatts/springboot_with_mybatis/controller/ApiError.java
+++ src/main/java/com/jigowatts/springboot_with_mybatis/controller/ApiError.java
@@ -1,7 +1,10 @@
 package com.jigowatts.springboot_with_mybatis.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
@@ -13,6 +16,30 @@ public class ApiError implements Serializable {
      *
      */
     private static final long serialVersionUID = 1L;
+
+    private static class Detail implements Serializable {
+
+        /**
+         *
+         */
+        private static final long serialVersionUID = 1L;
+
+        private final String target;
+        private final String message;
+
+        private Detail(String target, String message) {
+            this.target = target;
+            this.message = message;
+        }
+
+        public String getTarget() {
+            return this.target;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+    }
 
     private String message;
 
@@ -33,6 +60,17 @@ public class ApiError implements Serializable {
 
     public void setDocumentationUrl(String documentationUrl) {
         this.documentationUrl = documentationUrl;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private final List<Detail> details = new ArrayList<>();
+
+    public void addDetail(String target, String message) {
+        details.add(new Detail(target, message));
+    }
+
+    public List<Detail> getDetails() {
+        return details;
     }
     
 }
