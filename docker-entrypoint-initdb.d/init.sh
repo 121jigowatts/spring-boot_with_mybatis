5504a968e82881383ff8d0fa3a040c168bdd0633
soil <sh.yoshida@live.jp>
2020-02-04 01:04:45 +0900

    add init.sh

---
+++ docker-entrypoint-initdb.d/init.sh
@@ -0,0 +1,11 @@
+# コマンドが一つでもエラーになったらシェルを終了する
+set -e
+
+# psql -U <USER_NAME> <DB_NAME>
+psql -U admin admin << EOSQL
+CREATE TABLE IF NOT EXISTS messages (
+  id        SERIAL PRIMARY KEY,
+  text      VARCHAR(255)
+);
+
+EOSQL
