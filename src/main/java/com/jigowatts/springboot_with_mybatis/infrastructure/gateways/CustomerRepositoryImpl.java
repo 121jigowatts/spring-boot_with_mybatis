ad1d22d17bffb100ba403421b0312716826cf329
soil <sh.yoshida@live.jp>
2020-06-27 02:13:32 +0900

    add wiremock

--- src/main/java/com/jigowatts/springboot_with_mybatis/infrastructure/gateways/CustomerRepositoryImpl.java
+++ src/main/java/com/jigowatts/springboot_with_mybatis/infrastructure/gateways/CustomerRepositoryImpl.java
@@ -4,13 +4,20 @@ import java.util.Optional;
 
 import com.jigowatts.springboot_with_mybatis.domain.model.customer.Customer;
 import com.jigowatts.springboot_with_mybatis.domain.model.customer.CustomerRepository;
+import com.jigowatts.springboot_with_mybatis.infrastructure.gateways.client.CustomerInquiryClient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+    @Autowired
+    CustomerInquiryClient customerInquiryClient;
 
     @Override
     public Optional<Customer> findById(String customerId) {
-        // TODO Auto-generated method stub
-        return null;
+        return customerInquiryClient.getCustomer(customerId);
     }
     
 }
